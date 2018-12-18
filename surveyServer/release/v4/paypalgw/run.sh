14d59d1a2061aa5ad62a5f668bcb9fa7e23658a5
Su Pham Thanh <thanhsu604@gmail.com>
2018-12-18 20:43:10 +0700

    v4

--- surveyServer/release/v4/paypalgw/run.sh
+++ surveyServer/release/v4/paypalgw/run.sh
@@ -1,1 +1,1 @@
-java -cp *.jar com.survey.utils.controller.SurveyRun run com.survey.paypal.ServiceInit --cluster
+nohup java -cp *.jar com.survey.utils.controller.SurveyRun run com.survey.paypal.ServiceInit --cluster > ./log.txt 2>&1 &
